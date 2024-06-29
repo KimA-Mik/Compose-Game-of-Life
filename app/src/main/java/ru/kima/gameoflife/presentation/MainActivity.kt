6b8c06b0e72a74f9b9ddc166aa70c19765c9f112
KimA-Mik <ananim38@gmail.com>
2024-06-30 01:52:53 +0300

    Small refactor

--- app/src/main/java/ru/kima/gameoflife/presentation/MainActivity.kt
+++ app/src/main/java/ru/kima/gameoflife/presentation/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ru.kima.gameoflife.presentation.gameoflife.GameOfLifeScreen
+import ru.kima.gameoflife.presentation.screens.gameoflife.GameOfLifeScreen
 import ru.kima.gameoflife.presentation.theme.GameOfLifeTheme
 
 class MainActivity : ComponentActivity() {
