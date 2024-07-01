2205916dd5023b8ec834d3c482b9ce085bc0b9d7
KimA-Mik <ananim38@gmail.com>
2024-07-01 16:13:16 +0300

    Factor out field into StateFlow

--- app/src/main/java/ru/kima/gameoflife/domain/gameoflife/GameOfLife.kt
+++ app/src/main/java/ru/kima/gameoflife/domain/gameoflife/GameOfLife.kt
@@ -1,10 +1,11 @@
 package ru.kima.gameoflife.domain.gameoflife
 
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.isActive
 import kotlin.system.measureTimeMillis
 
 private const val TAG = "GameOfLife"
@@ -12,33 +13,34 @@ private const val TAG = "GameOfLife"
 class GameOfLife {
     val fieldWidth = 25
     val fieldHeight = 25
-    private var _field = IntArray(fieldWidth * fieldHeight)
-
+    private val _field: MutableStateFlow<List<Int>>
     init {
+        val fieldList = MutableList(fieldWidth * fieldHeight) { DEAD }
         val centerX = fieldWidth / 2
         val centerY = fieldHeight / 2
 
         var index = coordinatesToPIndex(centerX, centerY)
-        _field[index] = ALIVE
+        fieldList[index] = ALIVE
         index = coordinatesToPIndex(centerX + 1, centerY)
-        _field[index] = ALIVE
+        fieldList[index] = ALIVE
         index = coordinatesToPIndex(centerX, centerY + 1)
-        _field[index] = ALIVE
+        fieldList[index] = ALIVE
+        _field = MutableStateFlow(fieldList)
     }
+
+    val field = _field.asStateFlow()
 
 
     private fun coordinatesToPIndex(x: Int, y: Int): Int {
         return y * fieldWidth + x
     }
 
-    //TODO: Extract field to stateflow
-    fun gameLoop() = flow {
-        while (true) {
+    suspend fun gameLoop() = coroutineScope {
+        while (isActive) {
             val gameTime = measureTimeMillis {
-                _field = getNewField()
+                _field.value = getNewField()
             }
             Log.i(TAG, "Game tick took $gameTime ms")
-            emit(_field.toList())
 
 
             if (gameTime < SECOND_MILLIS) {
@@ -47,10 +49,10 @@ class GameOfLife {
                 Log.w(TAG, "Too slow")
             }
         }
-    }.flowOn(Dispatchers.Default)
+    }
 
-    private fun getNewField(): IntArray {
-        val newField = IntArray(_field.size)
+    private fun getNewField(): List<Int> {
+        val newField = MutableList(_field.value.size) { DEAD }
         for (y in 0 until fieldHeight) {
             for (x in 0 until fieldWidth) {
                 val neighbours = countNeighbours(x, y)
@@ -91,7 +93,7 @@ class GameOfLife {
                 val actualY = wrapCoordinate(checkY, 0, fieldHeight)
 
                 val index = coordinatesToPIndex(actualX, actualY)
-                if (_field[index] == ALIVE) {
+                if (_field.value[index] == ALIVE) {
                     res += 1
                 }
             }
